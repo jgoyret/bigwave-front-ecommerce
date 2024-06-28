0834ffbc963c49bd621983f264e6260bce668bb9
Adrian Rodriguez <rgadrian.93@gmail.com>
2024-06-28 11:56:31 -0300

    errors in app (routes)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import Home from "./pages/Home";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/Login";
-<<<<<<< Updated upstream
 import RegisterForm from "./pages/RegisterForm";
-=======
 import About from "./pages/About";
->>>>>>> Stashed changes
 
 function App() {
   const router = createBrowserRouter([
@@ -18,13 +15,12 @@ function App() {
       element: <Login />,
     },
     {
-<<<<<<< Updated upstream
       path: "/register",
       element: <RegisterForm />,
-=======
+    },
+    {
       path: "/about-this-project",
       element: <About />,
->>>>>>> Stashed changes
     },
   ]);
   return <RouterProvider router={router} />;
