409466a27e9b32c93986ff38f3996d4775d4cf1b
Juan Goyret <j.goyret1475@gmail.com>
2024-07-01 18:05:43 -0300

    hotfix on app for productview

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,49 @@
-import Home from './pages/Home';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Login from './pages/Login';
-import RegisterForm from './pages/RegisterForm';
-import About from './pages/About';
-import AllProducts from './pages/AllProducts';
-import Product from './pages/Product';
-import Category from './pages/Category';
-import Checkout from './pages/Checkout';
+import Home from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login";
+import RegisterForm from "./pages/RegisterForm";
+import About from "./pages/About";
+import AllProducts from "./pages/AllProducts";
+import ProductView from "./pages/ProductView";
+import Category from "./pages/Category";
+import Checkout from "./pages/Checkout";
 
 function App() {
-	const router = createBrowserRouter([
-		{
-			path: '/',
-			element: <Home />,
-		},
-		{
-			path: '/login',
-			element: <Login />,
-		},
-		{
-			path: '/register',
-			element: <RegisterForm />,
-		},
-		{
-			path: '/products',
-			element: <AllProducts />,
-		},
-		{
-			path: '/categories/:slug',
-			element: <Category />,
-		},
-		{
-			path: '/products/:slug',
-			element: <Product />,
-		},
-		{
-			path: '/about-this-project',
-			element: <About />,
-		},
-		{
-			path: '/checkout',
-			element: <Checkout />,
-		},
-	]);
-	return <RouterProvider router={router} />;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <RegisterForm />,
+    },
+    {
+      path: "/products",
+      element: <AllProducts />,
+    },
+    {
+      path: "/categories/:slug",
+      element: <Category />,
+    },
+    {
+      path: "/products/:slug",
+      element: <ProductView />,
+    },
+    {
+      path: "/about-this-project",
+      element: <About />,
+    },
+    {
+      path: "/checkout",
+      element: <Checkout />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
