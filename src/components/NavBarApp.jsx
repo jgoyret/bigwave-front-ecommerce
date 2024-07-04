a7ffce932539c2525a37c83d0519ca1f8149abee
Adrian Rodriguez <rgadrian.93@gmail.com>
2024-07-04 15:49:07 -0300

    errors in svg

--- src/components/NavBarApp.jsx
+++ src/components/NavBarApp.jsx
@@ -93,16 +93,16 @@ function NavBarApp() {
                   width="20px"
                 >
                   <path
-                    stroke-linejoin="round"
-                    stroke-linecap="round"
-                    stroke-width="2"
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2"
                     stroke="#000000"
                     d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
                   ></path>
                   <path
-                    stroke-linejoin="round"
-                    stroke-linecap="round"
-                    stroke-width="2"
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2"
                     stroke="#000000"
                     d="M22 22L20 20"
                   ></path>
