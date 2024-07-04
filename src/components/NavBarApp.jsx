b61157799a30bafa07ca33687ad6146610947896
sofiviera <sofiavierasosa@gmail.com>
2024-07-04 14:01:03 -0300

    search navbar

--- src/components/NavBarApp.jsx
+++ src/components/NavBarApp.jsx
@@ -80,16 +80,36 @@ function NavBarApp() {
               </NavDropdown>
               <Nav.Link href="/about-this-project">About this project</Nav.Link>
             </Nav>
-            <div className="group">
-              <svg className="icon" aria-hidden="true" viewBox="0 0 24 24">
-                <g>
-                  <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
-                </g>
-              </svg>
+            <div className="input-wrapper">
+              <button className="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  height="25px"
+                  width="25px"
+                >
+                  <path
+                    stroke-linejoin="round"
+                    stroke-linecap="round"
+                    stroke-width="2"
+                    stroke="#000000"
+                    d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
+                  ></path>
+                  <path
+                    stroke-linejoin="round"
+                    stroke-linecap="round"
+                    stroke-width="2"
+                    stroke="#000000"
+                    d="M22 22L20 20"
+                  ></path>
+                </svg>
+              </button>
               <input
-                placeholder="Search"
-                type="search"
+                placeholder="search.."
                 className="input"
+                name="text"
+                type="text"
               ></input>
             </div>
             <Link to={"/login"}>
