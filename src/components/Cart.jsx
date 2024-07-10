02dad0225c7f09618b0370d920cdb2ec06efdda2
nachokapre <nachok1000@gmail.com>
2024-07-10 12:06:35 -0300

    se anadieron checkeos antes de confirmar checkout, se arreglo profile, carrito vacio no compra

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,55 +1,60 @@
-import React from "react";
-import Offcanvas from "react-bootstrap/Offcanvas";
-import { Link } from "react-router-dom";
-import CartProduct from "./CartProduct";
-import "../styles/cart.css";
-import { useSelector } from "react-redux";
+import React from 'react';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { Link } from 'react-router-dom';
+import CartProduct from './CartProduct';
+import '../styles/cart.css';
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Cart({ show, handleClose }) {
-  const cart = useSelector((state) => state.cart);
+	const cart = useSelector((state) => state.cart);
 
-  const totalAmount = cart.reduce(
-    (total, item) => total + item.price * item.quantity,
-    0
-  );
+	const totalAmount = cart.reduce(
+		(total, item) => total + item.price * item.quantity,
+		0
+	);
 
-  return (
-    <>
-      <Offcanvas show={show} onHide={handleClose} placement="end">
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Shopping cart</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <div className="d-flex flex-column h-100">
-            <CartProduct />
-            <div className="mt-auto">
-              <hr />
-              <div className="d-flex justify-content-between">
-                <h6>Subtotal</h6>
-                <span>{totalAmount.toFixed(2)}</span>
-              </div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Tempora, aut.
-              </p>
-              <Link
-                to={"/checkout"}
-                className="btn btn-checkout-cart py-2 w-100"
-              >
-                Checkout
-              </Link>
-              <p className="text-center mt-3">
-                or
-                <button onClick={handleClose} className="btn">
-                  Continue Shopping
-                </button>
-              </p>
-            </div>
-          </div>
-        </Offcanvas.Body>
-      </Offcanvas>
-    </>
-  );
+	return (
+		<>
+			<Offcanvas show={show} onHide={handleClose} placement="end">
+				<Offcanvas.Header closeButton>
+					<Offcanvas.Title>Shopping cart</Offcanvas.Title>
+				</Offcanvas.Header>
+				<Offcanvas.Body>
+					<div className="d-flex flex-column h-100">
+						<CartProduct />
+						<div className="mt-auto">
+							<hr />
+							<div className="d-flex justify-content-between">
+								<h6>Subtotal</h6>
+								<span>{totalAmount.toFixed(2)}</span>
+							</div>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Tempora, aut.
+							</p>
+							<Link
+								to={cart.length > 0 && '/checkout'}
+								onClick={() => {
+									cart.length === 0 && toast.error('Cart is empty');
+								}}
+								className="btn btn-checkout-cart py-2 w-100"
+							>
+								Checkout
+							</Link>
+							<p className="text-center mt-3">
+								or
+								<button onClick={handleClose} className="btn">
+									Continue Shopping
+								</button>
+							</p>
+						</div>
+					</div>
+				</Offcanvas.Body>
+			</Offcanvas>
+		</>
+	);
 }
 
 export default Cart;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,262 +1,265 @@
-import NavBarApp from "../components/NavBarApp";
-import { useSelector } from "react-redux";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { Modal, Box, TextField, Typography, Button } from "@mui/material";
-import "../styles/profile.css";
-import { useNavigate } from "react-router-dom";
+import NavBarApp from '../components/NavBarApp';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Modal, Box, TextField, Typography, Button } from '@mui/material';
+import '../styles/profile.css';
+import { useNavigate } from 'react-router-dom';
 
 const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
+	position: 'absolute',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+	width: 400,
+	bgcolor: 'background.paper',
+	border: '2px solid #000',
+	boxShadow: 24,
+	p: 4,
 };
 
 function Profile() {
-  const navigate = useNavigate();
-  const token = useSelector((state) => state.user.token);
-  const [loggedUser, setLoggedUser] = useState();
+	const navigate = useNavigate();
+	const token = useSelector((state) => state.user.token);
+	const [loggedUser, setLoggedUser] = useState();
 
-  //open update profile modal
-  const [openModal, setOpenModal] = useState(false);
-  const handleOpenModal = () => setOpenModal(true);
-  const handleCloseModal = () => setOpenModal(false);
+	//open update profile modal
+	const [openModal, setOpenModal] = useState(false);
+	const handleOpenModal = () => setOpenModal(true);
+	const handleCloseModal = () => setOpenModal(false);
 
-  //open update password modal
-  const [openPasswordModal, setOpenPasswordModal] = useState(false);
-  const handleOpenPasswordModal = () => setOpenPasswordModal(true);
-  const handleClosePasswordModal = () => setOpenPasswordModal(false);
+	//open update password modal
+	const [openPasswordModal, setOpenPasswordModal] = useState(false);
+	const handleOpenPasswordModal = () => setOpenPasswordModal(true);
+	const handleClosePasswordModal = () => setOpenPasswordModal(false);
 
-  //form data
-  const [firstname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [password, setPassword] = useState("");
-  const [repeatedPassword, setRepeatedPassword] = useState("");
+	//form data
+	const [firstname, setFirstname] = useState('');
+	const [lastname, setLastname] = useState('');
+	const [password, setPassword] = useState('');
+	const [repeatedPassword, setRepeatedPassword] = useState('');
 
-  const updateUser = async () => {
-    try {
-      const response = await axios({
-        url: `${import.meta.env.VITE_API_URL}/users/my-profile`,
-        method: "PATCH",
-        headers: { Authorization: `Bearer ${token}` },
-        data: {
-          firstname: firstname || loggedUser.firstname,
-          lastname: lastname || loggedUser.lastname,
-        },
-      });
-      console.log(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	const updateUser = async () => {
+		try {
+			const response = await axios({
+				url: `${import.meta.env.VITE_API_URL}/users/my-profile`,
+				method: 'PATCH',
+				headers: { Authorization: `Bearer ${token}` },
+				data: {
+					firstname: firstname || loggedUser.firstname,
+					lastname: lastname || loggedUser.lastname,
+				},
+			});
+			console.log(response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  const updateUserPassword = async () => {
-    try {
-      if (password.length < 6) {
-        alert("Password must be at least 6 characters long");
-      } else {
-        if (password !== repeatedPassword) {
-          alert("Passwords do not match");
-        } else {
-          const response = await axios({
-            url: `${import.meta.env.VITE_API_URL}/users/my-profile`,
-            method: "PATCH",
-            headers: { Authorization: `Bearer ${token}` },
-            data: {
-              password: password,
-            },
-          });
-        }
+	const updateUserPassword = async () => {
+		try {
+			if (password.length < 6) {
+				alert('Password must be at least 6 characters long');
+			} else {
+				if (password !== repeatedPassword) {
+					alert('Passwords do not match');
+				} else {
+					const response = await axios({
+						url: `${import.meta.env.VITE_API_URL}/users/my-profile`,
+						method: 'PATCH',
+						headers: { Authorization: `Bearer ${token}` },
+						data: {
+							password: password,
+						},
+					});
+				}
 
-        console.log(response.data);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+				console.log(response.data);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  useEffect(() => {
-    if (!token) {
-      navigate("/login");
-    }
+	useEffect(() => {
+		if (!token) {
+			navigate('/login');
+		}
 
-    const getUser = async () => {
-      try {
-        const response = await axios({
-          url: `${import.meta.env.VITE_API_URL}/users/my-profile`,
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        });
-        setLoggedUser(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getUser();
-  }, []);
+		const getUser = async () => {
+			try {
+				const response = await axios({
+					url: `${import.meta.env.VITE_API_URL}/users/my-profile`,
+					method: 'GET',
+					headers: {
+						Authorization: `Bearer ${token}`,
+						'Content-Type': 'application/json',
+					},
+				});
+				setLoggedUser(response.data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getUser();
+	}, []);
 
-  return (
-    <>
-      <NavBarApp />
-      <div className="container my-profile-container">
-        <h3>
-          My profile{" "}
-          <span className="d-inline-block">
-            <button className="edit-button" onClick={handleOpenModal}>
-              <svg className="edit-svgIcon" viewBox="0 0 512 512">
-                <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path>
-              </svg>
-            </button>
-          </span>
-        </h3>
+	return (
+		<>
+			<NavBarApp />
+			<div className="container my-profile-container">
+				<h3>
+					My profile{' '}
+					<span className="d-inline-block">
+						<button className="edit-button" onClick={handleOpenModal}>
+							<svg className="edit-svgIcon" viewBox="0 0 512 512">
+								<path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path>
+							</svg>
+						</button>
+					</span>
+				</h3>
 
-        {loggedUser && (
-          <div className="container">
-            <div className="row">
-              <div className="col-12 col-md-6 px-0">
-                <h4 className="mt-3">Personal information</h4>
-                <p>
-                  <strong>Name:</strong> {loggedUser.firstname}
-                </p>
-                <p>
-                  <strong>Lastname:</strong> {loggedUser.lastname}
-                </p>
-                <p>
-                  <strong>Email:</strong> {loggedUser.email}
-                </p>
-              </div>
-            </div>
-            <button
-              type="button"
-              className="m-2 btn btn-success"
-              onClick={handleOpenPasswordModal}
-            >
-              Change password
-            </button>
-            <div className="mt-5">
-              <h3>My Orders</h3>
-            </div>
-            {loggedUser.Orders.map((order) => (
-              <div
-                key={order.id}
-                className="container border border-dark d-flex"
-              >
-                <div className="row">
-                  <div className="col fw-bold">Número:{order.id}</div>
-                  <div className="col-3">{order.createdAt.slice(0, 10)}</div>
-                  <div className="col">{order.address}</div>
-                  <div className="col-2">{order.status}</div>
-                  <div className="col">{order.totalAmount}</div>
-                  <ul>
-                    {order.products.map((item) => (
-                      <li key={item.id}>
-                        {item.name} x {item.quantity}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+				{loggedUser && (
+					<div className="container">
+						<div className="row">
+							<div className="col-12 col-md-6 px-0">
+								<h4 className="mt-3">Personal information</h4>
+								<p>
+									<strong>Name:</strong> {loggedUser.firstname}
+								</p>
+								<p>
+									<strong>Lastname:</strong> {loggedUser.lastname}
+								</p>
+								<p>
+									<strong>Email:</strong> {loggedUser.email}
+								</p>
+							</div>
+						</div>
+						<button
+							type="button"
+							className="m-2 btn btn-success"
+							onClick={handleOpenPasswordModal}
+						>
+							Change password
+						</button>
+						<div className="mt-5">
+							<h3>My Orders</h3>
+						</div>
+						{loggedUser.Orders.length === 0 && <h5>No orders yet</h5>}
+						{loggedUser.Orders.length > 0 &&
+							loggedUser.Orders.map((order) => (
+								<div
+									key={order.id}
+									className="container border border-dark d-flex"
+								>
+									<div className="row">
+										<div className="col fw-bold">Número:{order.id}</div>
+										<div className="col-3">{order.createdAt.slice(0, 10)}</div>
+										<div className="col">{order.address}</div>
+										<div className="col-2">{order.status}</div>
+										<div className="col">{order.totalAmount}</div>
+										<ul>
+											{order.products.map((item) => (
+												<li key={item.id}>
+													{item.name} x {item.quantity}
+												</li>
+											))}
+										</ul>
+									</div>
+								</div>
+							))}
+					</div>
+				)}
+				{console.log(loggedUser)}
+			</div>
 
-      {/* MODAL UPDATE PERFIL */}
-      <Modal
-        open={openModal}
-        onClose={handleCloseModal}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box component="form" sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Update your profile
-          </Typography>
-          <TextField
-            className="bg-transparent"
-            id="firstname"
-            label="First Name"
-            type="text"
-            variant="outlined"
-            defaultValue={loggedUser?.firstname}
-            fullWidth
-            onChange={(e) => setFirstname(e.target.value)}
-            sx={{ marginBottom: 5, marginTop: 5 }}
-          />
-          <TextField
-            className="bg-transparent"
-            id="lastname"
-            label="Last Name"
-            type="text"
-            variant="outlined"
-            defaultValue={loggedUser?.lastname}
-            onChange={(e) => setLastname(e.target.value)}
-            fullWidth
-          />
-          <button
-            type="submit"
-            onClose={handleCloseModal}
-            onClick={updateUser}
-            className=" w-25 button-add type1 mt-3 ml-3"
-          >
-            Guardar
-          </button>
-        </Box>
-      </Modal>
+			{/* MODAL UPDATE PERFIL */}
+			<Modal
+				open={openModal}
+				onClose={handleCloseModal}
+				aria-labelledby="modal-modal-title"
+				aria-describedby="modal-modal-description"
+			>
+				<Box component="form" sx={style}>
+					<Typography id="modal-modal-title" variant="h6" component="h2">
+						Update your profile
+					</Typography>
+					<TextField
+						className="bg-transparent"
+						id="firstname"
+						label="First Name"
+						type="text"
+						variant="outlined"
+						defaultValue={loggedUser?.firstname}
+						fullWidth
+						onChange={(e) => setFirstname(e.target.value)}
+						sx={{ marginBottom: 5, marginTop: 5 }}
+					/>
+					<TextField
+						className="bg-transparent"
+						id="lastname"
+						label="Last Name"
+						type="text"
+						variant="outlined"
+						defaultValue={loggedUser?.lastname}
+						onChange={(e) => setLastname(e.target.value)}
+						fullWidth
+					/>
+					<button
+						type="submit"
+						onClose={handleCloseModal}
+						onClick={updateUser}
+						className=" w-25 button-add type1 mt-3 ml-3"
+					>
+						Guardar
+					</button>
+				</Box>
+			</Modal>
 
-      {/* MODAL UPDATE PASSWORD */}
-      <Modal
-        open={openPasswordModal}
-        onClose={handleClosePasswordModal}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box component="form" sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Update your password
-          </Typography>
-          <TextField
-            className="bg-transparent"
-            id="newpassword"
-            label="New Password"
-            type="text"
-            variant="outlined"
-            defaultValue=""
-            fullWidth
-            onChange={(e) => setPassword(e.target.value)}
-            sx={{ marginBottom: 5, marginTop: 5 }}
-          />
-          <TextField
-            className="bg-transparent"
-            id="repeatedpassword"
-            label="Repeat new password"
-            type="text"
-            variant="outlined"
-            defaultValue=""
-            onChange={(e) => setRepeatedPassword(e.target.value)}
-            fullWidth
-          />
-          <button
-            type="submit"
-            onClose={handleClosePasswordModal}
-            onClick={updateUserPassword}
-            className=" w-25 button-add type1 mt-3 ml-3"
-          >
-            Guardar
-          </button>
-        </Box>
-      </Modal>
-    </>
-  );
+			{/* MODAL UPDATE PASSWORD */}
+			<Modal
+				open={openPasswordModal}
+				onClose={handleClosePasswordModal}
+				aria-labelledby="modal-modal-title"
+				aria-describedby="modal-modal-description"
+			>
+				<Box component="form" sx={style}>
+					<Typography id="modal-modal-title" variant="h6" component="h2">
+						Update your password
+					</Typography>
+					<TextField
+						className="bg-transparent"
+						id="newpassword"
+						label="New Password"
+						type="text"
+						variant="outlined"
+						defaultValue=""
+						fullWidth
+						onChange={(e) => setPassword(e.target.value)}
+						sx={{ marginBottom: 5, marginTop: 5 }}
+					/>
+					<TextField
+						className="bg-transparent"
+						id="repeatedpassword"
+						label="Repeat new password"
+						type="text"
+						variant="outlined"
+						defaultValue=""
+						onChange={(e) => setRepeatedPassword(e.target.value)}
+						fullWidth
+					/>
+					<button
+						type="submit"
+						onClose={handleClosePasswordModal}
+						onClick={updateUserPassword}
+						className=" w-25 button-add type1 mt-3 ml-3"
+					>
+						Guardar
+					</button>
+				</Box>
+			</Modal>
+		</>
+	);
 }
 
 export default Profile;
