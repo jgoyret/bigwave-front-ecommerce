a505f433843de26441d6f0d951ce99c12fab8798
Juan Goyret <j.goyret1475@gmail.com>
2024-07-15 22:53:36 -0300

    nada imporante

--- src/pages/OrderGreetings.jsx
+++ src/pages/OrderGreetings.jsx
@@ -68,7 +68,7 @@ function OrderGreetings() {
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
                           >
-                            <g id="SVGRepo_bgCarrier" strokewWidth="0"></g>
+                            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                             <g
                               id="SVGRepo_tracerCarrier"
                               strokeLinecap="round"
