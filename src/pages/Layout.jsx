e73d752bf378421bc7fdb0bacc88b8c9f262c682
nachokapre <nachok1000@gmail.com>
2024-07-16 18:04:31 -0300

    scroll to top incorporado

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,18 +1,20 @@
 // components/Layout.jsx
-import { Outlet } from "react-router-dom";
-import NavbarApp from "../components/NavBarApp";
-import Footer from "../components/Footer";
+import { Outlet } from 'react-router-dom';
+import NavbarApp from '../components/NavBarApp';
+import Footer from '../components/Footer';
+import ScrollToTop from '../components/ScrollToTop';
 
 function Layout() {
-  return (
-    <>
-      <NavbarApp />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<ScrollToTop />
+			<NavbarApp />
+			<main>
+				<Outlet />
+			</main>
+			<Footer />
+		</>
+	);
 }
 
 export default Layout;
