d0d797ee9b30d1ff11edc74ada152b4551c66a6f
Juan Goyret <j.goyret1475@gmail.com>
2024-07-17 03:58:36 -0300

    adding check of userexist in dbb

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,20 +1,50 @@
 // components/Layout.jsx
-import { Outlet } from 'react-router-dom';
-import NavbarApp from '../components/NavBarApp';
-import Footer from '../components/Footer';
-import ScrollToTop from '../components/ScrollToTop';
+import { Outlet } from "react-router-dom";
+import NavbarApp from "../components/NavBarApp";
+import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
+import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { logout } from "../redux/userSlice";
+import { clearCart } from "../redux/cartReducer";
+import axios from "axios";
 
 function Layout() {
-	return (
-		<>
-			<ScrollToTop />
-			<NavbarApp />
-			<main>
-				<Outlet />
-			</main>
-			<Footer />
-		</>
-	);
+  const dispatch = useDispatch();
+  const lUser = useSelector((state) => state.user);
+
+  React.useEffect(() => {
+    if (lUser.token) userExist();
+  }, []);
+
+  const userExist = async () => {
+    const response = await axios({
+      url: `${import.meta.env.VITE_API_URL}/users/my-profile`,
+      method: "get",
+      headers: {
+        Authorization: `Bearer ${lUser.token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.data) {
+      console.log("User exist");
+    } else {
+      dispatch(logout());
+      dispatch(clearCart());
+    }
+  };
+
+  return (
+    <>
+      <ScrollToTop />
+      <NavbarApp />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default Layout;
