9092e106e0ff403c1a269cf4c6fea1313aac1724
Adrian Rodriguez <rgadrian.93@gmail.com>
2024-07-18 00:47:01 -0300

    5 in a row team

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -102,8 +102,8 @@ function About() {
             <div className="heading">
               <h3>Meet the Team</h3>
             </div>
-            <div className="d-flex">
-              <div className="row justify-content-evenly w-75 mt-5 m-auto ">
+            <div className="d-flex justify-content-evenly">
+              <div className="row mt-5">
                 <div className="card-client col-12 col-sm-6 col-md-2 col-lg-2 col-xl-2">
                   <div className="user-picture hover-img">
                     <img
